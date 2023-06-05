423aac1be1284b5001f35a3a78a45157d6c61c2e
Igor Trentini <igortrentini.2004@gmail.com>
2023-06-05 17:09:40 -0300

    wip: Abstração de geração de sessão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
 import { Home } from '@/app/components/Home';
 import { NextPage } from 'next';
-import { ReactElement } from 'react';
+import { FC, ReactElement, useCallback, useMemo } from 'react';
 import { Menu } from '@/app/components/Menu';
 import { MenuIds } from '@/app/components/consts';
 import { Technologies } from '@/app/components/Technologies';
 
+interface Section {
+    id: MenuIds;
+    component: ReactElement;
+}
+
 const Page: NextPage = (): ReactElement => {
+    const sections: Section[] = useMemo(() => {
+        return [
+            {
+                id: MenuIds.Home,
+                component: <Home />,
+            },
+            {
+                id: MenuIds.Technologies,
+                component: <Technologies />,
+            },
+        ];
+    }, []);
+
+    const getBgColor: (index: number) => string = useCallback((index: number): string => {
+        if (index % 2 == 0) return 'bg-blayout';
+
+        return 'bg-blayout-2';
+    }, []);
+
     return (
         <main className="max-h-[100vh] md:grid md:grid-cols-12 md:grid-flow-row overflow-y-scroll flex flex-col">
             <div className="h-24 md:h-auto md:relative flex flex-row md:flex-col justify-center md:justify-normal items-center order-last md:order-first bg-primary">
@@ -13,13 +37,13 @@ const Page: NextPage = (): ReactElement => {
             </div>
 
             <div className="h-[calc('100vh - 6rem')] md:h-auto md:col-span-11 overflow-y-scroll">
-                <section id={MenuIds.Home} className="min-h-screen py-6 px-4 lg:px-32 flex content-center bg-blayout">
-                    <Home />
-                </section>
-
-                <section id={MenuIds.Technologies} className="min-h-screen py-6 px-4 lg:px-32 bg-blayout-2">
-                    <Technologies />
-                </section>
+                {sections.map(({ id, component }, index) => {
+                    return (
+                        <section key={id} className={`min-h-screen py-6 px-4 lg:px-32 ${getBgColor(index)}`}>
+                            {component}
+                        </section>
+                    );
+                })}
             </div>
         </main>
     );
